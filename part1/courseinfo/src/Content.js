b1a153cbf22509267c342c541b1de5391f813732
Andrés <aac.devs@gmail.com>
2021-10-02 09:40:34 -0300

    part1 exercise1.4 finished

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -3,9 +3,9 @@ import Part from './Part';
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.p1} exercises={props.e1} />
-      <Part part={props.p2} exercises={props.e2} />
-      <Part part={props.p3} exercises={props.e3} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   );
 };
