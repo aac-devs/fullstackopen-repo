655e6db94eff576e4a3a55f69dd98a1aae530de4
Andrés <aac.devs@gmail.com>
2021-10-02 09:34:25 -0300

    part1 exercise1.3 finished

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,25 +6,31 @@ import Total from './Total';
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header title={course} />
       <Content
-        p1={part1}
-        e1={exercises1}
-        p2={part2}
-        e2={exercises2}
-        p3={part3}
-        e3={exercises3}
+        p1={part1.name}
+        e1={part1.exercises}
+        p2={part2.name}
+        e2={part2.exercises}
+        p3={part3.name}
+        e3={part3.exercises}
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
