ee22fa6de3164a48d810236377bb6a886a55244b
Andrés <aac.devs@gmail.com>
2021-10-02 08:15:38 -0300

    part1 exercise1.1 finished

---
+++ part1/courseinfo/src/Content.js
@@ -0,0 +1,17 @@
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.p1} {props.e1}
+      </p>
+      <p>
+        {props.p2} {props.e2}
+      </p>
+      <p>
+        {props.p3} {props.e3}
+      </p>
+    </div>
+  );
+};
+
+export default Content;
