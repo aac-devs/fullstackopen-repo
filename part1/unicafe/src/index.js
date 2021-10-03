a37b32868bc63c92221a54558e2aeb9b0254a248
Andrés <aac.devs@gmail.com>
2021-10-03 03:25:49 -0300

    part1 exercise1.8 finished

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,27 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Statistics = ({ good, neutral, bad }) => {
+  const allComments = good + neutral + bad;
+  let average = 0;
+  let percentage = 0;
+  if (allComments !== 0) {
+    average = (good - bad) / allComments;
+    percentage = (good * 100) / allComments;
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {allComments}</p>
+      <p>average {average}</p>
+      <p>positive {percentage} %</p>
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -11,27 +32,13 @@ const App = () => {
     setComment(newValue);
   };
 
-  const allComments = good + neutral + bad;
-  let average = 0;
-  let percentage = 0;
-  if (allComments !== 0) {
-    average = (good - bad) / allComments;
-    percentage = (good * 100) / allComments;
-  }
-
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={commentHandler(setGood, good + 1)}>good</button>
       <button onClick={commentHandler(setNeutral, neutral + 1)}>neutral</button>
       <button onClick={commentHandler(setBad, bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {allComments}</p>
-      <p>average {average}</p>
-      <p>positive {percentage} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
