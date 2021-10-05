09fb8fa882d88fcbc90097b88cbfe7b4405fd4dd
Andrés <aac.devs@gmail.com>
2021-10-05 09:47:46 -0300

    part2 exercise2.18 finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -29,17 +29,34 @@ const App = () => {
 
     const names = persons.map((person) => person.name.toLowerCase());
     if (names.includes(newName.trim().toLowerCase())) {
-      alert(`${newName.trim()} is already added to phonebook`);
+      if (
+        window.confirm(
+          `${newName.trim()} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const personToUpdate = persons.filter(
+          (person) => person.name === newName
+        )[0];
+        const { id, name } = personToUpdate;
+        personService.update(id, { name, number: newNumber }).then((resp) => {
+          const updatedList = persons.map((person) => {
+            if (person.id === id) return resp;
+            return person;
+          });
+          setPersons(updatedList);
+        });
+        return;
+      }
       return;
+    } else {
+      personService
+        .create({ name: newName.trim(), number: newNumber })
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson));
+        });
     }
 
     setFilteredName('');
-
-    personService
-      .create({ name: newName.trim(), number: newNumber })
-      .then((returnedPerson) => {
-        setPersons(persons.concat(returnedPerson));
-      });
   };
 
   const handleDelete = (id) => {
