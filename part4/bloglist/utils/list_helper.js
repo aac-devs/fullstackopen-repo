115e4da2cb677ee81f2fd3fbf46c3f974ac603b5
Andrés <aac.devs@gmail.com>
2021-10-10 08:53:48 -0300

    part4 exercise4.4 finished

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,17 @@
 const dummy = (blogs) => {
   // ...
   return 1;
-}
+};
+
+const totalLikes = (blogs) => {
+  let total = 0;
+  for (let blog of blogs) {
+    total += blog.likes;
+  }
+  return total;
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes,
+};
