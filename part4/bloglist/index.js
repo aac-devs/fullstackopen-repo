af7bd747a2a7d957b3b82efd421ad90b23f0bce8
Andrés <aac.devs@gmail.com>
2021-10-10 06:43:11 -0300

    part4 ex4.2 utils/config.js added

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,9 +1,9 @@
-const dotenv = require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const config = require('./utils/config.js');
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -14,14 +14,14 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+//const mongoUrl = process.env.MONGODB_URI;
+
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
-  console.log('app get');
   Blog.find({}).then((blogs) => {
     response.json(blogs);
   });
@@ -35,7 +35,6 @@ app.post('/api/blogs', (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
