c7c6a9c0dfb4f5e49d264ee6e4f192f546ea6d9b
Andrés <aac.devs@gmail.com>
2021-10-10 07:12:36 -0300

    part4 exercise4.1-2 finished

---
+++ part4/bloglist/app.js
@@ -0,0 +1,18 @@
+const logger = require('./utils/logger.js');
+const config = require('./utils/config.js');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bloglistRouter = require('./controllers/bloglists.js');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then((resp) => logger.info('connected to mongodb'))
+  .catch((error) => logger.info(error.message));
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', bloglistRouter);
+
+module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,22 +1,10 @@
+const app = require('./app.js');
 const http = require('http');
-const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
-const cors = require('cors');
 const config = require('./utils/config.js');
 const logger = require('./utils/logger.js');
-const bloglistRouter = require('./controllers/bloglists.js');
 
-mongoose
-  .connect(config.MONGODB_URI)
-  .then((resp) => logger.info('connected to mongodb'))
-  .catch((error) => logger.info(error.message));
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', bloglistRouter);
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
