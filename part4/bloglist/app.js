36022825168e128f200adc2e86b1892cad89b552
Andrés <aac.devs@gmail.com>
2021-10-13 12:11:35 -0300

    part4 exercise4.15 finished

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const bloglistRouter = require('./controllers/bloglists.js');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 
@@ -16,6 +17,8 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', bloglistRouter);
+app.use('/api/users', usersRouter);
+
 app.use(middleware.errorHandler);
 
 module.exports = app;
