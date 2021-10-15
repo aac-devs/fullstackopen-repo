eddfc79fd342f5a01099dfa90002f26d2be26f0e
Andrés <aac.devs@gmail.com>
2021-10-15 09:46:15 -0300

    part4 exercise4.17 finished

--- part4/bloglist/controllers/bloglists.js
+++ part4/bloglist/controllers/bloglists.js
@@ -1,17 +1,26 @@
 const bloglistRouter = require('express').Router();
 const Blog = require('../models/bloglist.js');
+const User = require('../models/user.js');
 
 bloglistRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 bloglistRouter.post('/', async (request, response) => {
-  const blogObj = request.body;
-  if (!blogObj?.likes) blogObj.likes = 0;
-  const blog = new Blog(blogObj);
-  const result = await blog.save();
-  response.status(201).json(result);
+  const { title, author, url, likes: lks, userId } = request.body;
+  const user = await User.findById(userId);
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: !lks ? 0 : lks,
+    user: user._id,
+  });
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await User.findByIdAndUpdate(user._id, user);
+  response.status(201).json(savedBlog);
 });
 
 bloglistRouter.delete('/:id', async (request, response) => {
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -22,7 +22,11 @@ usersRouter.post('/', async (request, response, next) => {
 });
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs', { content: 1, date: 1 });
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+  });
   response.json(users);
 });
 
